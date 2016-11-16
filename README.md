a1e78be09ea8daf5fbf588b5aea962e88c2b2b4e
Thomas Goldbrunner <thomas.goldbrunner@tum.de>
2016-11-16 10:33:57 +0100

    added support for spotify and rhythmbox

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # clementine-pause-on-lock
-Simple script that pauses [Clementine](https://github.com/clementine-player/Clementine) when the screen gets locked and resumes playback once the screen is un-locked again.
+Simple script that pauses your music player, when the screen gets locked and resumes playback once the screen is un-locked again.
+
+Currently Supported Players:
+- [Clementine](https://www.clementine-player.org/)
+- [Spotify](https://www.spotify.com/us/download/linux/)
+- [Rhythmbox](https://wiki.gnome.org/Apps/Rhythmbox)
+
+Easily extendable for any player that uses the [MPRIS D-Bus Interface](https://specifications.freedesktop.org/mpris-spec/latest/).
+
 
 Tested with Unity on Ubuntu 16.04.
-
-
-
--- clementine-pause-on-lock.sh
+++ clementine-pause-on-lock.sh
@@ -1,23 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 dbus-monitor --session "type='signal',interface='com.ubuntu.Upstart0_6'" | \
 (
-  PAUSED_ON_LOCK=0 #varibale to check, whether we paused on lock
+  # variables to check, whether we paused on lock
+  CLEMENTINE_PAUSED=0
+  SPOTIFY_PAUSED=0
+  RHYTHMBOX_PAUSED=0
+
   while true; do
     read X
-    # lock & pause
+    # pause on lock
     if echo $X | grep 'desktop-lock' &> /dev/null; then
-      if qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus | grep 'Playing' &> /dev/null; then
+      # clementine
+      if qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus 2>/dev/null | grep 'Playing' &> /dev/null; then
         dbus-send --print-reply --dest=org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause &> /dev/null
-        PAUSED_ON_LOCK=1
+        CLEMENTINE_PAUSED=1
+      fi
+      # spotify
+      if qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus 2> /dev/null | grep 'Playing' &> /dev/null; then
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause &> /dev/null
+        SPOTIFY_PAUSED=1
+      fi
+      # rhythmbox
+      if qdbus org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus 2> /dev/null | grep 'Playing' &> /dev/null; then
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause &> /dev/null
+        RHYTHMBOX_PAUSED=1
       fi
     fi
-    # unlock & resume
+    # resume on unlock
     if echo $X | grep 'desktop-unlock' &> /dev/null; then
-      if [ "$PAUSED_ON_LOCK" -eq "1" ] &> /dev/null; then
+      # clemetine
+      if [ $CLEMENTINE_PAUSED -eq 1 ] &> /dev/null; then
         if qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus | grep 'Paused' &> /dev/null; then
           dbus-send --print-reply --dest=org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play &> /dev/null
-          PAUSED_ON_LOCK=0
+          CLEMENTINE_PAUSED=0
+        fi
+      fi
+      # spotify
+      if [ $SPOTIFY_PAUSED -eq 1 ] &> /dev/null; then
+        if qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus | grep 'Paused' &> /dev/null; then
+          dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play &> /dev/null
+          SPOTIFY_PAUSED=0
+        fi
+      fi
+      # rhythmbox
+      if [ $RHYTHMBOX_PAUSED -eq 1 ] &> /dev/null; then
+        if qdbus org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus | grep 'Paused' &> /dev/null; then
+          dbus-send --print-reply --dest=org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play &> /dev/null
+          RHYTHMBOX_PAUSED=0
         fi
       fi
     fi

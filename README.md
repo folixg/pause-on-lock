a7b84da7df75a427fda8168939a3155069402e23
Thomas Goldbrunner <thomas.goldbrunner@tum.de>
2017-10-19 09:13:47 +0200

    Limit support without playerctl to Rhythmbox

--- README.md
+++ README.md
@@ -4,17 +4,9 @@ resumes playback once the screen is un-locked again.
 
 ### Supported players
 
-If you have [playerctl](https://github.com/acrisci/playerctl) installed (which I
-recommend), it supports all players that playerctl can handle.
-
-Without playerctl the following players are supported:
-- [Clementine](https://www.clementine-player.org/)
-- [Spotify](https://www.spotify.com/us/download/linux/)
-- [Rhythmbox](https://wiki.gnome.org/Apps/Rhythmbox)
-- [Quod Libet](https://quodlibet.readthedocs.io/en/latest/)
-- [Google Play Music Desktop Player](https://www.googleplaymusicdesktopplayer.com)
-
-Easily extendable for any player that uses the [MPRIS D-Bus Interface](https://specifications.freedesktop.org/mpris-spec/latest/).
+If you have [playerctl](https://github.com/acrisci/playerctl) installed (which is
+recommended), it supports all players that playerctl can handle.
+Without playerctl the only supported player is [Rhythmbox](https://wiki.gnome.org/Apps/Rhythmbox).
 
 ### Usage
 1. Put ```pause-on-lock.sh``` in any directory of your choosing.
--- pause-on-lock.sh
+++ pause-on-lock.sh
@@ -2,30 +2,15 @@
 
 dbus-monitor --session "type='signal',interface='com.canonical.Unity.Session'" | \
 (
-  # use playerctl if it is installed
-  if [ "$(playerctl --version 2>/dev/null)" ]; then
-    PLAYERCTL=1
-  else
-    PLAYERCTL=0
-  fi
-
-  # variables to check, whether we paused on lock
-  if [ $PLAYERCTL -eq 1 ]; then
-    PAUSED_PLAYER="none"
-  else
-    CLEMENTINE_PAUSED=0
-    SPOTIFY_PAUSED=0
-    RHYTHMBOX_PAUSED=0
-    QUODLIBET_PAUSED=0
-    GPMDP_PAUSED=0
-  fi
+  # variable to check, whether we paused on lock
+  PAUSED_PLAYER="none"
 
   while true; do
     read X
     # pause on lock
-    if echo $X | grep 'Locked' &> /dev/null; then
+    if echo "$X" | grep 'Locked' &> /dev/null; then
       # with playerctl
-      if [ $PLAYERCTL -eq 1 ]; then
+      if [ "$(playerctl --version 2>/dev/null)" ]; then
         # get list of running players
         read -a players <<< $(playerctl --list-all)
         # check whether a player is playing
@@ -39,76 +24,27 @@ dbus-monitor --session "type='signal',interface='com.canonical.Unity.Session'" |
         done
       # without playerctl  
       else
-        # clementine
-        if qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus 2>/dev/null | grep 'Playing' &> /dev/null; then
-          dbus-send --print-reply --dest=org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause &> /dev/null
-          CLEMENTINE_PAUSED=1
-        fi
-        # spotify
-        if qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus 2> /dev/null | grep 'Playing' &> /dev/null; then
-          dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause &> /dev/null
-          SPOTIFY_PAUSED=1
-        fi
-        # rhythmbox
         if qdbus org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus 2> /dev/null | grep 'Playing' &> /dev/null; then
           dbus-send --print-reply --dest=org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause &> /dev/null
-          RHYTHMBOX_PAUSED=1
-        fi
-        # quod libet
-        if qdbus net.sacredchao.QuodLibet /net/sacredchao/QuodLibet net.sacredchao.QuodLibet.IsPlaying 2> /dev/null | grep 'true' &> /dev/null; then
-          dbus-send --print-reply --dest=net.sacredchao.QuodLibet /net/sacredchao/QuodLibet net.sacredchao.QuodLibet.Pause &> /dev/null
-          QUODLIBET_PAUSED=1
-        fi
-        # google play music desktop player
-        if qdbus org.mpris.MediaPlayer2.google-play-music-desktop-player /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus 2>/dev/null | grep 'Playing' &> /dev/null; then
-          dbus-send --print-reply --dest=org.mpris.MediaPlayer2.google-play-music-desktop-player /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause &> /dev/null
-          GPMDP_PAUSED=1
+          PAUSED_PLAYER="rhythmbox"
         fi
       fi
     fi
     # resume on unlock
-    if echo $X | grep 'Unlocked' &> /dev/null; then
+    if echo "$X" | grep 'Unlocked' &> /dev/null; then
       # with playerctl
-      if [ $PLAYERCTL -eq 1 ]; then
+      if [ "$(playerctl --version 2>/dev/null)" ]; then
+        # did we pause a player on lock?
         if [ "$PAUSED_PLAYER" != "none" ]; then
           playerctl --player="$PAUSED_PLAYER" play
+          PAUSED_PLAYER="none"
         fi
       # without playerctl
       else
-        # clemetine
-        if [ $CLEMENTINE_PAUSED -eq 1 ] &> /dev/null; then
-          if qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus | grep 'Paused' &> /dev/null; then
-            dbus-send --print-reply --dest=org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play &> /dev/null
-            CLEMENTINE_PAUSED=0
-          fi
-        fi
-        # spotify
-        if [ $SPOTIFY_PAUSED -eq 1 ] &> /dev/null; then
-          if qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus | grep 'Paused' &> /dev/null; then
-            dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play &> /dev/null
-            SPOTIFY_PAUSED=0
-          fi
-        fi
-        # rhythmbox
-        if [ $RHYTHMBOX_PAUSED -eq 1 ] &> /dev/null; then
-          if qdbus org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus | grep 'Paused' &> /dev/null; then
-            dbus-send --print-reply --dest=org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play &> /dev/null
-            RHYTHMBOX_PAUSED=0
-          fi
-        fi
-        # rhythmbox
-        if [ $QUODLIBET_PAUSED -eq 1 ] &> /dev/null; then
-          if qdbus net.sacredchao.QuodLibet /net/sacredchao/QuodLibet net.sacredchao.QuodLibet.IsPlaying 2> /dev/null | grep 'false' &> /dev/null; then
-            dbus-send --print-reply --dest=net.sacredchao.QuodLibet /net/sacredchao/QuodLibet net.sacredchao.QuodLibet.Play &> /dev/null
-            QUODLIBET_PAUSED=0
-          fi
-        fi
-        # google play music desktop player
-        if [ $GPMDP_PAUSED -eq 1 ] &> /dev/null; then
-          if qdbus org.mpris.MediaPlayer2.google-play-music-desktop-player /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus | grep 'Paused' &> /dev/null; then
-            dbus-send --print-reply --dest=org.mpris.MediaPlayer2.google-play-music-desktop-player /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play &> /dev/null
-            GPMDP_PAUSED=0
-          fi
+        # did we pause on lock?
+        if [ "$PAUSED_PLAYER" = "rhythmbox" ] ; then
+          dbus-send --print-reply --dest=org.mpris.MediaPlayer2.rhythmbox /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play &> /dev/null
+          PAUSED_PLAYER="none"          
         fi
       fi
     fi
